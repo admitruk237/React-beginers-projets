571279c2b80d4dd133946d6ce0653e4cca0217c8
Andrii Dmytruk <admitruk237@gmail.com>
2024-08-16 15:12:37 +0200

    carrency-convertor is ready

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
-import { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './index.css';
+import { Block } from './components/Block';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const onClickPlus = () => {
-    setCount(count + 1);
-  };
-  const onClickMinus = () => {
-    if (count > 0) {
-      setCount(count - 1);
+  const [fromCurrency, setFromCurrency] = useState('USD');
+  const [toCurrency, setToCurrency] = useState('PLN');
+  const [fromPrice, setFromPrice] = useState(1); // Змінив початкове значення на 1
+  const [toPrice, setToPrice] = useState(0);
+  const ratesRef = useRef({});
+
+  useEffect(() => {
+    const getCurrency = async () => {
+      try {
+        const response = await fetch(
+          'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
+        );
+        const data = await response.json();
+        const ratesObject = data.reduce((acc, currency) => {
+          acc[currency.cc] = currency.rate;
+          return acc;
+        }, {});
+
+        ratesObject['UAH'] = 1; // Додамо гривню як базову валюту
+        ratesRef.current = ratesObject;
+        onChangeFromPrice(1);
+      } catch (error) {
+        console.error('Error fetching currency data:', error);
+      }
+    };
+
+    getCurrency();
+  }, []);
+
+  const onChangeFromPrice = (value) => {
+    if (ratesRef.current[fromCurrency] && ratesRef.current[toCurrency]) {
+      const priceInUAH = value * ratesRef.current[fromCurrency];
+      const result = priceInUAH / ratesRef.current[toCurrency];
+      setFromPrice(value);
+      setToPrice(result.toFixed(3));
     }
   };
+
+  const onChangeToPrice = (value) => {
+    if (ratesRef.current[fromCurrency] && ratesRef.current[toCurrency]) {
+      const priceInUAH = value * ratesRef.current[toCurrency];
+      const result = priceInUAH / ratesRef.current[fromCurrency];
+      setToPrice(value);
+      setFromPrice(result.toFixed(3));
+    }
+  };
+
+  useEffect(() => {
+    if (fromCurrency && toCurrency) {
+      onChangeFromPrice(fromPrice);
+    }
+  }, [fromCurrency]);
+
+  useEffect(() => {
+    if (fromCurrency && toCurrency) {
+      onChangeToPrice(toPrice);
+    }
+  }, [toCurrency]);
+
   return (
     <div className="App">
-      <div>
-        <h2>Counter:</h2>
-        <h1>{count}</h1>
-        <button onClick={onClickMinus} className="minus">
-          - Minus
-        </button>
-        <button onClick={onClickPlus} className="plus">
-          Plus +
-        </button>
-      </div>
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
